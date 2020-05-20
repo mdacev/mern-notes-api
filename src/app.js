0fd14178568c5350b70f95f67d29f8825bb17550
mariano <marianodacev@gmail.com>
2020-05-20 18:10:01 -0300

    h

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
     */
    res.header("Access-Control-Allow-Origin", '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, X-WP-Nonce, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.header('Cache-Control', 'max-age=0,no-cache,no-store,post-check=0,pre-check=0,must-revalidate');
     res.header('Expires', '-1');
