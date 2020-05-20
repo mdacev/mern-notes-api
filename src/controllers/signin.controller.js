cf5b70300c4fcb2f017b6cf98c953f9c7f592eea
mariano <marianodacev@gmail.com>
2020-05-20 16:53:01 -0300

    jwt

--- src/controllers/signin.controller.js
+++ src/controllers/signin.controller.js
@@ -17,7 +17,10 @@ signinCrtl.signin = async (req, res, next) => {
         if(!validPassword){
             return res.status(220).json({auth:false , token: null, status:202 , msg:'Password incorrecta.'});
         }
-        const token = jwt.sign({id: user._id} , config.secret, {expiresIn: config.expiresIn});
+        //const token = jwt.sign({id: user._id} , config.secret, {expiresIn: config.expiresIn});
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYzU3NGZkZDExZThlMDAxNzU5Y2E0OSIsImlhdCI6MTU5MDAwNDE3MywiZXhwIjoxNTkwMjYzMzczfQ.4aT4PHEHo1h8uh7JxWVaIHRCDftCnHNW84MZFz45QqY';
+
+        
         return res.json({auth: true, token, userId: user._id, username: user.username, avatar: user.image_url});
         
     }
