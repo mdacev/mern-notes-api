a90db96f7b871ae6cbbbaf42489b1e257a7df97b
mariano <marianodacev@gmail.com>
2020-05-20 14:06:01 -0300

    2

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+/*
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -20,7 +21,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});
+});*/
 
 /*app.use(function (req, res, next) {
 
