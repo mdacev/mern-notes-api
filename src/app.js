ab2f4548cb9f6a77642d265035104f57366df3bb
mariano <marianodacev@gmail.com>
2020-05-20 16:31:46 -0300

    h

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
 // settings
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(cors());
 /*app.use(function (req, res, next) {
 
     res.header("Access-Control-Allow-Origin", '*');
