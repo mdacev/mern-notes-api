9cdc38671862bebe3b1200867397e64888f6b431
mariano <marianodacev@gmail.com>
2020-05-20 14:42:21 -0300

    4

--- src/app.js
+++ src/app.js
@@ -16,10 +16,10 @@ app.use(express.urlencoded({extended:false}));
 // Configurar cabeceras y cors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    //res.header("Access-Control-Allow-Credentials", true);
-    //res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, x-access-token, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    //res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    //res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, x-access-token, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 
